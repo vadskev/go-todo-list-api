8f08b97dce72b3c203a801b6fd68b3a0c471c01e
Vadim Vaskevich <vadskev@ya.ru>
2024-08-09 12:28:05 +0300

    fix: change auth

--- internal/middleware/auth/auth.go
+++ internal/middleware/auth/auth.go
@@ -1,30 +1,27 @@
 package auth
 
 import (
+	"github.com/vadskev/go_final_project/internal/config"
 	"net/http"
-
-	"github.com/vadskev/go_final_project/internal/lib/logger"
-	"github.com/vadskev/go_final_project/internal/lib/utils"
 )
 
-func New(pass string) func(next http.Handler) http.Handler {
-	const op = "middleware.auth.New"
+func New(pass config.PasswordConfig) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if len(pass) > 0 {
+			if len(pass.GetPass()) > 0 {
 				cookie, err := r.Cookie("token")
 				if err != nil {
 					http.Error(w, "Authentification required", http.StatusUnauthorized)
 					return
 				}
-				hash := utils.CreateHash(pass)
+
+				hash := pass.CreateHash(pass.GetPass())
 
 				if cookie.Value != hash {
 					http.Error(w, "Authentification required", http.StatusUnauthorized)
 					return
 				}
 			}
-			logger.Info(op)
 			next.ServeHTTP(w, r)
 		})
 	}
